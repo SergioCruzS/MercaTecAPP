8b1f4e684911062893b2e395e0053286e5acfe24
Sergio Cruz <sergio91@hotmail.es>
2022-10-16 20:52:22 -0500

    Avance login and register

---
+++ app/src/main/java/com/siuuu/mercatec/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.siuuu.mercatec.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.siuuu.mercatec.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
